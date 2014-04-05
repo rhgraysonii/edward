6cc564a06125025327f588ead87126ad8b596d7f
Bobby Grayson <bobbygrayson@gmail.com>
2014-04-05 17:36:53 -0400

    setup with readline/editline/history and remove hello world test file

--- REPL.c
+++ REPL.c
@@ -1,31 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <editline/readline.h>
-#include <editline/history.h>
-
-/* static buffer to limit input size */
-static char input[2048];
 
 int main(int argc, char** argv) {
-  /* print version info and tell how to exit */
-  puts("Edward Version 0.0.1\n");
-  
-  puts("Press Ctrl+C to exit\n");
-
-  /* Endless loop to run main REPL */
+     
+  /* version/exit info */
+  puts("Edward Version 0.0.1");
+  puts("Press Ctrl+c to Exit\n");
+         
+  /* endless loop for main REPL */
   while (1) {
-    /* prompt output */
-    char* input = readline("Edward> ");
-    
-    /* add history */
+    /* output prompt and read line */
+    char* input = readline("edward> ");
+                  
+    /* put input in history  */
     add_history(input);
+                      
+    /* Echo input back */    
+    printf("No you're a %s\n", input);
 
-    /* echo input */
-    printf("No you're a %s", input);
-
-    /* free retrieved input */
-    free(input)
+    /* free input */
+    free(input);
   }
   return 0;
 }
-
