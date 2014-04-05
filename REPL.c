aabf3f3bf140c700cb94595f86e53bffa515ef08
Bobby Grayson <bobbygrayson@gmail.com>
2014-04-05 16:03:01 -0400

    fix minor typo

--- REPL.c
+++ REPL.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <editline/readline.h>
-#include <editeline/history.h>
+#include <editline/history.h>
 
 /* static buffer to limit input size */
 static char input[2048];
