54dee8b3e2a14960875b41159547d9609fe47a08
Bobby Grayson <bobbygrayson@gmail.com>
2014-04-05 16:02:04 -0400

    add in history, line editing, and implement stdlib

--- REPL.c
+++ REPL.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <editline/readline.h>
+#include <editeline/history.h>
 
 /* static buffer to limit input size */
 static char input[2048];
@@ -6,20 +9,23 @@ static char input[2048];
 int main(int argc, char** argv) {
   /* print version info and tell how to exit */
   puts("Edward Version 0.0.1\n");
+  
   puts("Press Ctrl+C to exit\n");
 
   /* Endless loop to run main REPL */
   while (1) {
     /* prompt output */
-    fputs("~edward:", stdout);
-
-    /* check user input size to compare with buffer */
-    fgets(input, 2048, stdin);
+    char* input = readline("Edward> ");
+    
+    /* add history */
+    add_history(input);
 
     /* echo input */
     printf("No you're a %s", input);
-  }
 
+    /* free retrieved input */
+    free(input)
+  }
   return 0;
 }
 
